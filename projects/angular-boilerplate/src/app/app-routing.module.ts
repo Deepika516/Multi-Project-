15854e2b8a8c2bd01f74c7b37b2f22e8a4f882a1
deepika516 <deepika.mahindroo@sourcefuse.com>
2023-04-20 13:57:37 +0530

    initial commmit

---
+++ projects/angular-boilerplate/src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { environment } from '@main-project/boilerplate/env/';
+import {
+  AuthGuard,
+  LoggedInGuard,
+} from 'projects/shared-library/src/lib/shared/core/auth';
+import { EnvResolverService } from 'projects/shared-library/src/lib/shared/core/services';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    resolve: {
+      env: EnvResolverService,
+    },
+    loadChildren: () =>
+      import('projects/shared-library/src/lib/auth/auth.module').then(
+        (m) => m.AuthModule
+      ),
+    // canActivate: [LoggedInGuard],
+  },
+  {
+    path: 'main',
+    resolve: {
+      env: EnvResolverService,
+    },
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+    // canActivate: [AuthGuard],
+  },
+  {
+    path: '',
+    redirectTo: environment.homePath,
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: environment.homePath,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ projects/angular-boilerplate/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ComponentBaseDirective } from 'projects/shared-library/src/lib/shared/core';
+import { TranslationService } from 'projects/shared-library/src/lib/shared/core';
+import { IconPacksManagerService } from 'projects/shared-library/src/lib/theme';
+
+@Component({
+  selector: 'boiler-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent extends ComponentBaseDirective implements OnInit {
+  title = 'boiler-plate-ui';
+  constructor(
+    private readonly iconMgr: IconPacksManagerService,
+    private readonly languageTranslateService: TranslationService
+  ) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.languageTranslateService.init();
+    this.iconMgr.registerFontAwesome();
+    this.iconMgr.registerSvgs();
+  }
+}
